b71bfff51a784cbe19e38c9ad7d7a9b1d91615b7
Jonah Groendal <13501607+JonahGroendal@users.noreply.github.com>
2020-11-15 18:31:12 -0500

    Update truffle and recompile

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 var Asn1Decode = artifacts.require("Asn1Decode");
-var NodePtr = artifacts.require("NodePtr");
+// var NodePtr = artifacts.require("NodePtr");
 
 module.exports = function(deployer, network) {
-  deployer.deploy(NodePtr);
-  deployer.link(NodePtr, Asn1Decode);
+  // I believe these are not needed since all the libraries are compiled into one because all their functions are declared as `internal`
+  // In fact this library shouldn't even be deployed at all since it will be compiled into the contract that uses it
+  // deployer.deploy(NodePtr);
+  // deployer.link(NodePtr, Asn1Decode);
+
   deployer.deploy(Asn1Decode);
 };
